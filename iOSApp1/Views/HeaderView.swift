193a5a584066048e89a1e5323bd53917ad1b0d98
FoziaAkhtar <fozia.shah0001@gmail.com>
2026-05-24 23:08:24 -0400

    Changes made

--- iOSApp1/Views/HeaderView.swift
+++ iOSApp1/Views/HeaderView.swift
@@ -1,34 +1,45 @@
-//
+
+// ====================================
 //  HeaderView.swift
 //  iOSApp1
-//
-//  Created by Fozia on 2026-05-24.
-//
+//  Created by Fozia
+// ====================================
 
 import SwiftUI
 
+// Header section shown at the top of each order page
 struct HeaderView: View {
     
+    // Stores the person's name
     let personName: String
     
     var body: some View {
+        
+        // Main vertical layout
         VStack {
+            
+            // Display Persons Name
             Text(personName)
                 .font(.largeTitle)
                 .fontWeight(.bold)
             
+            // Display Page icons indicator
             HStack {
+                
                 Image(systemName: "1.circle")
                 Image(systemName: "2.circle")
                 Image(systemName: "3.circle")
                 Image(systemName: "4.circle")
             }
+            
+            // Icons styling
             .font(.title2)
             .foregroundColor(.red)
         }
     }
 }
 
+// SwiftUI Previews
 #Preview(traits: .sizeThatFitsLayout) {
     HeaderView(personName: "Fozia")
 }
